4588c529eac041937fc7a9f71a536d97a524ab16
ranjit485 <patilranjit485@gmail.com>
2023-10-06 19:40:31 -1200

    Sub panel added - 13:7 PM by ranjit patil

--- src/sem_3.java
+++ src/sem_3.java
@@ -3,26 +3,41 @@ import java.awt.*;
 
 public class sem_3 extends Header{
 
-    JPanel sem3,s1,s2,s3,s4,s5;
+    JPanel sem3,p1,p2,p3,p4,p5;
     JButton sb1,sb2,sb3,sb4,sb5;
     sem_3(){
         System.out.println("Sem-3");
-        sem3 = new JPanel(new GridLayout(3,1));
+        sem3 = new JPanel(new GridLayout(5,1));
         sem3.setBackground(Color.DARK_GRAY);
-
-        JPanel p1 = new JPanel();
-        JPanel p2 = new JPanel();
-        p1.setBorder(BorderFactory.createEmptyBorder(1,2,2,2));
-
-        p2.setBackground(Color.red);
-        p1.setBackground(Color.BLUE);
-//        p1.setSize(385,50);
-//        p2.setSize(385,100);
-
-
-        sem3.add(p1,SwingConstants.CENTER);
-        sem3.add(p2);
         sem3.setSize(395,390);
+
+        p1 = new JPanel();
+        p2 = new JPanel();
+        p3 = new JPanel();
+        p4 = new JPanel();
+        p5 = new JPanel();
+
+        p1.setBackground(Color.red);
+        p2.setBackground(Color.BLUE);
+        p3.setBackground(Color.WHITE);
+        p4.setBackground(Color.GREEN);
+        p5.setBackground(Color.MAGENTA);
+
+        p1.setBounds(10,10,380,50);
+        p2.setBounds(10,70,380,50);
+        p3.setBounds(10,135,380,50);
+        p4.setBounds(10,200,380,50);
+        p5.setBounds(10,265,380,50);
+
+
+        sem3.add(p1,0);
+        sem3.add(p2,1);
+        sem3.add(p3,2);
+        sem3.add(p4,3);
+        sem3.add(p5,4);
+
+
+
         body.add(sem3,BorderLayout.CENTER);
     }
 }
